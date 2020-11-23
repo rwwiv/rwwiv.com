9756c79078d7fea5028d2c5cea355c909cece007
William Wernert <william.wernert@gmail.com>
2020-11-22 21:25:38 -0500

    Fix break on about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,7 +5,7 @@
       <v-lazy-image :src="require('@/assets/img/full/DSF4898.jpg')" :src-placeholder="require('@/assets/img/thumbs/DSF4898.jpg')" class="mt-4 px-3 h-full" height="250" width="250" />
       <div class="my-6 sm:my-8">Self Portrait - me, 2020</div>
     </div>
-    <div class="text-white text-center w-5/6">
+    <div class="text-white text-center w-5/6 mb-12">
       Open source software developer for <a href="https://securityonionsolutions.com/" target="_blank" class="underline">Security Onion Solutions</a>
       <br>
       &bull;
@@ -14,7 +14,9 @@
       <br>
       &bull;
       <br>
-      Amateur photographer (Canon A-1, Fujifilm X-T3)
+      Amateur photographer
+      <br>
+      (Canon A-1, Fujifilm X-T3)
     </div>
   </div>
 </template>
