208e328af5f643e5bd0fb38dd538aa5ac03fac01
William Wernert <william.wernert@gmail.com>
2020-11-22 20:45:44 -0500

    Proficiency graph fixes

--- src/components/ExperienceDots.vue
+++ src/components/ExperienceDots.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="flex flex-row">
-    <div class="mr-2 w-1/2 xxs:w-1/4 sm:w-1/3 text-right">
+    <div class="mr-2 w-1/2 xxs:w-1/3 text-right">
       <slot />
     </div>
-   <div class="flex flex-row w-1/2 xxs:w-1/4 sm:w-2/3 font-mono	">
+   <div class="flex flex-row w-1/2 xxs:w-2/3 font-mono	">
       <div v-for="index in num" :key="index" class="font-extrabold">
         &bull;
       </div>
