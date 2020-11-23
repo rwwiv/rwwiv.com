5899bfe901966719aaa28cc90cff1f939218885d
William Wernert <william.wernert@gmail.com>
2020-11-22 21:13:31 -0500

    Remove unnecessary key directive from prof graph

--- src/components/ExperienceDots.vue
+++ src/components/ExperienceDots.vue
@@ -1,13 +1,15 @@
 <template>
+<!-- eslint-disable vue/no-unused-vars -->
+<!-- eslint-disable vue/require-v-for-key -->
   <div class="flex flex-row">
     <div class="mr-2 w-1/2 xxs:w-1/3 text-right">
       <slot />
     </div>
    <div class="flex flex-row w-1/2 xxs:w-2/3 font-mono	">
-      <div v-for="index in num" :key="index" class="font-extrabold">
+      <div v-for="index in num" class="font-extrabold">
         &bull;
       </div>
-      <div v-for="index in remainder" :key="index" class="text-gray-400">
+      <div v-for="index in remainder" class="text-gray-400">
         -
       </div>
    </div>
