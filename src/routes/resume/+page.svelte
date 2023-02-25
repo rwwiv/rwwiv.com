d7eb494fd7b3344ab51324aa40c02f33c86b9db8
William Wernert <william.wernert@gmail.com>
2023-02-25 10:26:55 -0500

    WIP -> DRAFT

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -13,7 +13,7 @@
 
 <div class="flex flex-col items-center">
 	<div class="text-center mb-6">
-		<div class="text-4xl text-red-400">WIP</div>
+		<div class="text-4xl text-red-400">DRAFT</div>
 		<br />
 		<div class="text-4xl">William Wernert</div>
 		<div class="text-lg font-light">Full Stack Developer</div>
