5f940871d8e00692d5d5d7c2f38451f2aee2d9b9
William Wernert <william.wernert@gmail.com>
2023-02-26 21:20:01 -0500

    Set static height on image to prevent reflow

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -13,7 +13,7 @@
 			id="polaroid"
 			class="flex flex-col items-center justify-center rounded-sm img-wrapper bg-black dark:bg-white"
 		>
-			<img src={me} alt="Self portrait" class="mt-4 px-3" />
+			<img src={me} alt="Self portrait" class="mt-4 px-3" height="350px" />
 			<div class="h-[75px]" />
 		</div>
 		<div class="flex flex-col space-y-6 w-[20em] mb-12">
