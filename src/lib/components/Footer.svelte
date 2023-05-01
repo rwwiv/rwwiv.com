b9849a75df8e019485b8fa6de1a46906662a58d9
William Wernert <william.wernert@gmail.com>
2023-05-01 10:39:26 -0400

    Move other links to bottom of page

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -2,8 +2,23 @@
 	import DarkMode from "./DarkMode.svelte";
 </script>
 
-<div class="flex flex-col items-center justify-center my-4 space-y-4">
+<div class="flex flex-col items-center justify-center my-4">
 	<DarkMode />
 
-	<div class="text-sm">&copy; 2023 William Wernert</div>
+	<div id="bottom-nav" class="flex justify-center space-x-4 mt-4 text-sm">
+		<a href="https://calendly.com/rwwiv/30min" target="_blank" rel="noreferrer">Calendly</a>
+		<a href="https://www.instagram.com/rwwiv/" target="_blank" rel="noreferrer">Instagram</a>
+		<a href="https://github.com/rwwiv" target="_blank" rel="noreferrer">GitHub</a>
+		<a href="https://www.linkedin.com/in/rwwiv/" target="_blank" rel="noreferrer">LinkedIn</a>
+	</div>
+
+	<div class="flex space-x-2 text-sm mt-2">
+		<div>&copy; 2023 William Wernert</div>
+	</div>
 </div>
+
+<style lang="postcss">
+	#bottom-nav a {
+		@apply hover:dark:text-neutral-400 hover:text-neutral-500 focus:underline focus:outline-none;
+	}
+</style>
--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -4,15 +4,9 @@
 
 <div id="nav">
 	<a href="/" class:active={$page.url.pathname === "/"} class="text-5xl mt-3">RWWIV</a>
-	<div class="flex justify-center space-x-4 mt-2 text-lg">
-		<a href="https://calendly.com/rwwiv/30min" target="_blank" rel="noreferrer">Calendly</a>
+	<div class="flex justify-center space-x-4 mt-2 text-2xl">
 		<a href="/about" class:active={$page.url.pathname === "/about"}>About</a>
 		<a href="/resume" class:active={$page.url.pathname === "/resume"}>Resume</a>
-	</div>
-	<div class="flex justify-center space-x-4 mt-5">
-		<a href="https://www.instagram.com/rwwiv/" target="_blank" rel="noreferrer">Instagram</a>
-		<a href="https://github.com/rwwiv" target="_blank" rel="noreferrer">GitHub</a>
-		<a href="https://www.linkedin.com/in/rwwiv/" target="_blank" rel="noreferrer">LinkedIn</a>
 	</div>
 </div>
 
@@ -22,10 +16,10 @@
 	}
 
 	#nav a {
-		@apply font-bold text-gray-500 hover:text-gray-600;
+		@apply font-bold text-neutral-500 dark:text-neutral-500 hover:text-neutral-700 dark:hover:text-neutral-400 focus:underline focus:outline-none;
 	}
 
 	#nav a.active {
-		@apply text-black dark:text-white hover:text-gray-700 dark:hover:text-gray-300;
+		@apply text-neutral-900 dark:text-neutral-100 hover:text-neutral-700 dark:hover:text-neutral-300;
 	}
 </style>
